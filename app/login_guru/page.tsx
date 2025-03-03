4ae595dbec985f88725b1f2db9ed5a4bacf0e8d9
dimazzfathi <dimazzfathi@gmail.com>
2025-03-03 08:35:44 +0700

    D: login guru

--- app/login_guru/page.tsx
+++ app/login_guru/page.tsx
@@ -6,86 +6,73 @@ import Cookies from 'js-cookie';
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const LoginFormGuru = () => {
+  const [email, setEmail] = useState('');
+  const [pas, setPas] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${baseUrl}/api/login`, {
-        username,
-        password
+      const response = await axios.post(`${baseUrl}/api/login-guru`, {
+        email,
+        pas
       });
-      console.log('Full Response:', response);  // Log seluruh respons
-    console.log('Token:', response.data.token);  // Pastikan token ada
-    console.log('Nama Admin:', response.data.nama_admin);  // Pastikan nama admin ada
-    
+      console.log('Full Response:', response);
+      console.log('Token:', response.data.token);
+      console.log('Nama Guru:', response.data.nama_guru);
+      
       if (response.status === 200) {
         // Menyimpan token ke dalam cookie dengan waktu kadaluarsa 5 menit
         const expireDate = new Date();
         expireDate.setTime(expireDate.getTime() + (60 * 60 * 1000)); // 5 menit dalam milidetik
-        Cookies.set('token', response.data.token, { expires: expireDate});
-        Cookies.set('nama_admin', response.data.data.nama_admin, { expires: expireDate});
-        Cookies.set('status', response.data.data.status, { expires: expireDate});
-        Cookies.set('id_admin', response.data.data.id_admin, { expires: expireDate});
-        // **Cek status dan redirect sesuai peran**
+        Cookies.set('token', response.data.token, { expires: expireDate });
+        Cookies.set('nama_guru', response.data.data.nama_guru, { expires: expireDate });
+        Cookies.set('walas', response.data.data.walas, { expires: expireDate });
+        Cookies.set('id_guru', response.data.data.id_guru, { expires: expireDate });
+        
+        // Redirect ke halaman dashboard guru
         window.location.href = '../';
       }
     } catch (error: unknown) {
       if (error instanceof Error && 'response' in error) {
-        // Pastikan error adalah instance Error dan memiliki properti `response`
-        const axiosError = error as { response: { status: number } }; // Casting dengan lebih aman
+        const axiosError = error as { response: { status: number } };
         if (axiosError.response) {
           console.error('Respons error:', axiosError.response);
           if (axiosError.response.status === 401) {
-            setErrorMessage('Username atau password salah.');
+            setErrorMessage('Email atau password salah.');
           } else {
             setErrorMessage('Terjadi kesalahan pada server.');
           }
         }
       } else {
-        // Tangani error yang bukan dari Axios (misalnya, error biasa atau jaringan)
         console.error('Kesalahan tanpa respons:', error);
         setErrorMessage('Tidak dapat menghubungi server.');
       }
-    // catch (error) {
-    //     if (error.response) {
-    //       console.error('Respons error:', error.response);
-    //       if (error.response.status === 401) {
-    //         setErrorMessage('Username atau password salah.');
-    //       } else {
-    //         setErrorMessage('Terjadi kesalahan pada server.');
-    //       }
-    //     } else {
-    //       console.error('Kesalahan tanpa respons:', error);
-    //       setErrorMessage('Tidak dapat menghubungi server.');
-    //     }
-    //   }
     }
   }
+
   return (
     <main className="bg-pageBg bg-cover bg-center bg-no-repeat min-h-screen">
       <div className="flex justify-center items-center w-full min-h-screen bg-black bg-opacity-15">
         <aside className="bg-white w-full max-w-md rounded-xl bg-opacity-20 shadow-lg shadow-black mx-4 sm:mx-8 md:mt-16 lg:mt-24">
-          <h1 className="text-center text-black font-light text-4xl bg-teal-400 rounded-t-xl p-4 font-times">Login</h1>
+          <h1 className="text-center text-black font-light text-4xl bg-teal-400 rounded-t-xl p-4 font-times">Login Guru</h1>
           <form className="flex flex-col p-6 space-y-4" onSubmit={handleSubmit}>
             <input
-              type="text"
-              name="username"
-              placeholder="Username"
+              type="email"
+              name="email"
+              placeholder="Email"
               className="py-2 px-3 w-full text-black text-lg font-light outline-none rounded-md"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="password"
-              name="password"
+              name="pas"
               placeholder="Password"
               className="py-2 px-3 w-full text-black text-lg font-light outline-none rounded-md"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={pas}
+              onChange={(e) => setPas(e.target.value)}
             />
             {errorMessage && (
               <p className="text-red-500 text-sm">{errorMessage}</p>
@@ -108,4 +95,4 @@ const LoginForm = () => {
   )
 }
 
-export default LoginForm;
+export default LoginFormGuru;
