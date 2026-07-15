d67648d8ec9b36bd99d6f3eb90823f74d1323cc0
lukimme <lukmanhakim8832@gmail.com>
2026-07-15 12:47:42 +0700

    build 3 lulusan

--- app/version.js
+++ app/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "1.6.13";
+export const APP_VERSION = "1.6.14";
 export const FOOTER_TEXT = `Developed by IT BMDev and SIJA major.`;
 export const Footer_2 = `Copyright © 2023 - ${new Date().getFullYear()} | Budi Mulia Vocational High School | Versi: ${APP_VERSION}`;
